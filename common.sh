879adaed6a71ac3afadf0f5d95e2aa76d8af5de0
naveen p <naveenpendekatla0407@gmail.com>
2023-05-19 19:52:31 +0530

    common

--- common.sh
+++ common.sh
@@ -21,55 +21,55 @@ fun_nodjs () {
 
   fun_head setup nodejs
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>logfile
-  fun_stat_check
+  fun_stat_check $?
   fun_head install nodejs
   yum install nodejs -y &>>logfile
-  fun_stat_check
+  fun_stat_check $?
 
   fun_app_prereq
 
   fun_head download dependencies
   npm install &>>logfile
-  fun_stat_check
+  fun_stat_check $?
   fun_head setup systemd service
   cp /home/centos/roboshop-scripting/${component}.service /etc/systemd/system/${component}.service &>>logfile
-  fun_stat_check
+  fun_stat_check $?
   fun_head load service
   systemctl daemon-reload &>>logfile
-  fun_stat_check
+  fun_stat_check $?
   fun_head start service
   systemctl enable ${component} &>>logfile
   systemctl restart ${component} &>>logfile
-  fun_stat_check
+  fun_stat_check $?
 
 }
 fun_schema() {
   fun_head setup mongodb repo file
   cp /home/centos/roboshop-scripting/mongo.repo /etc/yum.repos.d/mongo.repo &>>logfile
-  fun_stat_check
+  fun_stat_check $?
   fun_head install mongodb
 
   yum install mongodb-org-shell -y &>>logfile
-  fun_stat_check
+  fun_stat_check $?
   fun_head load schema
   mongo --host mongodb.naveendevops2.online </app/schema/${component}.js &>>logfile
-  fun_stat_check
+  fun_stat_check $?
 
 }
 fun_app_prereq () {
   useradd ${app_user}
   fun_head create app directory
-  fun_stat_check
+  fun_stat_check $?
 
   rm -rf /app
   mkdir /app
-  fun_stat_check
+  fun_stat_check $?
   fun_head   download app content
   curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
-  fun_stat_check
+  fun_stat_check $?
   fun_head extract content
 
   cd /app
   unzip /tmp/${component}.zip
-  fun_stat_check
+  fun_stat_check $?
 }
--- frontend.sh
+++ frontend.sh
@@ -6,25 +6,25 @@ source ${script_path}/common.sh
 
 fun_head install nginx
 yum install nginx -y &>>logfile
-fun_stat_check
+fun_stat_check $?
 fun_head start nginx
 systemctl enable nginx
 systemctl start nginx &>>logfile
-fun_stat_check
+fun_stat_check $?
 fun_head remove default content
 rm -rf /usr/share/nginx/html/* &>>logfile
-fun_stat_check
+fun_stat_check $?
 fun_head download app content
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>logfile
-fun_stat_check
+fun_stat_check $?
 fun_head extract content
 cd /usr/share/nginx/html &>>logfile
 unzip /tmp/frontend.zip &>>logfile
-fun_stat_check
+fun_stat_check $?
 fun_head setup configuration
 cp /home/centos/roboshop-scripting/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>logfile
-fun_stat_check
+fun_stat_check $?
 fun_head start nginx &>>logfile
 systemctl restart nginx &>>logfile
-fun_stat_check
+fun_stat_check $?
