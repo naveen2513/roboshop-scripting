bd2b6b14ecdfed5fa509d416b536c770b2612222
naveen p <naveenpendekatla0407@gmail.com>
2023-05-19 09:04:27 +0530

    frontend

---
+++ frontend.sh
@@ -0,0 +1,17 @@
+echo "\e[35m>>>>>>>>>>>> install nginx<<<<<<<<<<<<\e[0m"
+yum install nginx -y
+echo "\e{35m>>>>>>>>>>>> start nginx<<<<<<<<<<<<\e[0m"
+systemctl enable nginx
+systemctl start nginx
+echo "\e{35m>>>>>>>>>>>> remove default content<<<<<<<<<<<<\e[0m"
+rm -rf /usr/share/nginx/html/*
+echo "\e{35m>>>>>>>>>>>> download frontend content<<<<<<<<<<<<\e[0m"
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+echo "\e{35m>>>>>>>>>>>> extract frontend content<<<<<<<<<<<<\e[0m"
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+echo "\e{35m>>>>>>>>>>>> setup systemd service<<<<<<<<<<<<\e[0m"
+cp /home/centos/roboshop-scripting/roboshop.conf /etc/systemd/system/roboshop.conf
+echo "\e{35m>>>>>>>>>>>> restart nginx<<<<<<<<<<<<\e[0m"
+systemctl restart nginx
