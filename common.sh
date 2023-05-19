288825045299b71f25f29407a461d4a608d4ae51
naveen p <naveenpendekatla0407@gmail.com>
2023-05-19 19:49:31 +0530

    common

--- common.sh
+++ common.sh
@@ -6,15 +6,16 @@ fun_head () {
     echo -e "\e[35m>>>>>>>>>>>> $1<<<<<<<<<<<<\e[0m"
 
 
+
 }
 fun_stat_check () {
-  if [$1 -eq 0]; then
-    echo -e "\e[32msuccess\e[0m"
-  else
-    echo -e "\e[32mfailure\e[0m"
-    echo "refer the /tmp/roboshop.log for more information"
-    exit 1
-  fi
+  if [ $1 -eq 0 ]; then
+      echo -e "\e[32mSUCCESS\e[0m"
+    else
+      echo -e "\e[31mFAILURE\e[0m"
+      echo "Refer the log file /tmp/roboshop.log for more information"
+      exit 1
+    fi
 }
 fun_nodjs () {
 
