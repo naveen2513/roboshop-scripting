0d5828cfa29582af984faedb8995cda933a26ad9
naveen p <naveenpendekatla0407@gmail.com>
2023-05-19 09:42:06 +0530

    catalogue

--- frontend.sh
+++ frontend.sh
@@ -5,13 +5,13 @@ systemctl enable nginx
 systemctl start nginx
 echo -e "\e{35m>>>>>>>>>>>> remove default content<<<<<<<<<<<<\e[0m"
 rm -rf /usr/share/nginx/html/*
-echo -e  "\e{35m>>>>>>>>>>>> download frontend content<<<<<<<<<<<<\e[0m"
+echo -e "\e{35m>>>>>>>>>>>> download frontend content<<<<<<<<<<<<\e[0m"
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 echo -e "\e{35m>>>>>>>>>>>> extract frontend content<<<<<<<<<<<<\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-echo -e  "\e{35m>>>>>>>>>>>> setup systemd service<<<<<<<<<<<<\e[0m"
+echo -e "\e{35m>>>>>>>>>>>> setup systemd service<<<<<<<<<<<<\e[0m"
 cp /home/centos/roboshop-scripting/roboshop.conf /etc/nginx/default.d/roboshop.conf
-echo -e  "\e{35m>>>>>>>>>>>> restart nginx<<<<<<<<<<<<\e[0m"
+echo -e "\e{35m>>>>>>>>>>>> restart nginx<<<<<<<<<<<<\e[0m"
 systemctl restart nginx
