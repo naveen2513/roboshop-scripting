13389d0347a80789fe55fac3baf2c6ffc5666705
naveen p <naveenpendekatla0407@gmail.com>
2023-05-19 11:46:45 +0530

    cart

--- redis.sh
+++ redis.sh
@@ -5,7 +5,7 @@ yum module enable redis:remi-6.2 -y
 echo -e "\e[35m>>>>>>>>>>>> install redis<<<<<<<<<<<<\e[0m"
 yum install redis -y
 echo -e "\e[35m>>>>>>>>>>>> install redis<<<<<<<<<<<<\e[0m"
-sed -i -e "s|127.0.0.1|0.0.0.0|g" /etc/redis.conf
+sed -i -e "s|127.0.0.1|0.0.0.0|g" /etc/redis.conf /etc/redis/redis.conf
 echo -e "\e[35m>>>>>>>>>>>> start redis<<<<<<<<<<<<\e[0m"
 systemctl enable redis
-systemctl start redis
+systemctl restart redis
