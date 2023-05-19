4f19f8e1489412edc16fba4f3f06a4a0f00029e2
naveen p <naveenpendekatla0407@gmail.com>
2023-05-19 19:55:23 +0530

    common

--- frontend.sh
+++ frontend.sh
@@ -4,27 +4,27 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
 
-fun_head install nginx
+fun_head "install nginx"
 yum install nginx -y &>>logfile
 fun_stat_check $?
-fun_head start nginx
+fun_head "start nginx"
 systemctl enable nginx
 systemctl start nginx &>>logfile
 fun_stat_check $?
-fun_head remove default content
+fun_head "remove default content"
 rm -rf /usr/share/nginx/html/* &>>logfile
 fun_stat_check $?
-fun_head download app content
+fun_head "download app content"
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>logfile
 fun_stat_check $?
-fun_head extract content
+fun_head "extract content"
 cd /usr/share/nginx/html &>>logfile
 unzip /tmp/frontend.zip &>>logfile
 fun_stat_check $?
-fun_head setup configuration
+fun_head 'setup configuration'
 cp /home/centos/roboshop-scripting/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>logfile
 fun_stat_check $?
-fun_head start nginx &>>logfile
+fun_head "start nginx "
 systemctl restart nginx &>>logfile
 fun_stat_check $?
