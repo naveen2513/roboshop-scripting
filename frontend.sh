2e4c77e4bc73d2bdc92bcf6389f86a619ca7222d
naveen p <naveenpendekatla0407@gmail.com>
2023-05-19 10:09:53 +0530

    frontend

--- frontend.sh
+++ frontend.sh
@@ -1,16 +1,17 @@
 echo -e "\e[35m>>>>>>>>>>>> install nginx<<<<<<<<<<<<\e[0m"
 yum install nginx -y
-echo -e "\e{35m>>>>>>>>>>>> start nginx<<<<<<<<<<<<\e[0m"
+echo -e "\e[35m>>>>>>>>>>>> start nginx<<<<<<<<<<<<\e[0m"
 systemctl enable nginx
 systemctl start nginx
-echo -e "\e{35m>>>>>>>>>>>> remove default content<<<<<<<<<<<<\e[0m"
+echo -e "\e[35m>>>>>>>>>>>> remove default content<<<<<<<<<<<<\e[0m"
 rm -rf /usr/share/nginx/html/*
-echo -e "\e{35m>>>>>>>>>>>> download frontend content<<<<<<<<<<<<\e[0m"
+echo -e "\e[35m>>>>>>>>>>>> download app content<<<<<<<<<<<<\e[0m"
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-echo -e "\e{35m>>>>>>>>>>>> extract frontend content<<<<<<<<<<<<\e[0m"
+echo -e "\e[35m>>>>>>>>>>>> extract content<<<<<<<<<<<<\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-echo -e "\e{35m>>>>>>>>>>>> setup systemd service<<<<<<<<<<<<\e[0m"
-#echo -e "\e{35m>>>>>>>>>>>> restart nginx<<<<<<<<<<<<\e[0m"
+echo -e "\e[35m>>>>>>>>>>>> setup configuration<<<<<<<<<<<<\e[0m"
+cp /home/centos/roboshop-scripting/roboshop.conf /etc/nginx/default.d/roboshop.conf
+echo -e "\e[35m>>>>>>>>>>>> start nginx<<<<<<<<<<<<\e[0m"
 systemctl restart nginx
