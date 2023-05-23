6d03266b11723ffd0ca0c32cae067dea34741e9c
naveen p <naveenpendekatla0407@gmail.com>
2023-05-23 16:36:59 +0530

    terraform

--- payment.sh
+++ payment.sh
@@ -1,9 +1,9 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-rabbitmq_appuser_password=$1
+rabbitmq_password=$1
 
-if [ -z "$rabbitmq_appuser_password" ]; then
+if [ -z "${rabbitmq_password}" ]; then
   echo Input Roboshop Appuser Password Missing
   exit 1
 fi
