3231c23e0a06dec2b2e847a33bf06e883651714a
naveen p <naveenpendekatla0407@gmail.com>
2023-06-02 07:08:42 +0530

    ansible catalogue

--- common.sh
+++ common.sh
@@ -59,7 +59,7 @@ func_app_prereq() {
   func_stat_check $?
 
   func_print_head "Download Application Content"
-  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>$log_file
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${{component}}.zip &>>$log_file
   func_stat_check $?
 
   func_print_head "Extract Application Content"
