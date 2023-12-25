c96581f0de41bc503836df34a671cd33312aa9c5
naveen p <naveenpendekatla0407@gmail.com>
2023-12-25 20:07:12 +0530

    frontend

--- frontend.sh
+++ frontend.sh
@@ -27,6 +27,5 @@ func_stat_check $?
 func_print_head "Start Nginx"
 systemctl enable nginx &>>$log_file
 systemctl restart nginx &>>$log_file
-systemctl start nginx &>>$log_file
 
 func_stat_check $?
