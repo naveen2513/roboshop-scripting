13f0c338265ccfd0150f592035ba4a3c7f009291
naveen p <naveenpendekatla0407@gmail.com>
2023-12-25 20:05:53 +0530

    jenkins jobs

--- frontend.sh
+++ frontend.sh
@@ -27,4 +27,6 @@ func_stat_check $?
 func_print_head "Start Nginx"
 systemctl enable nginx &>>$log_file
 systemctl restart nginx &>>$log_file
+systemctl start nginx &>>$log_file
+
 func_stat_check $?
